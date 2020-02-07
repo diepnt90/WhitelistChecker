7a87f6127772db9143c7de6612ab0a42038ca711
diepnt <diep90.nt@gmail.com>
2020-02-07 13:55:03 +0700

    Update WhitelistChecker.cs

--- WhitelistChecker.cs
+++ WhitelistChecker.cs
@@ -42,54 +42,5 @@ namespace WindowsFormsApp1
                 System.Diagnostics.Process.Start("page.html");
             }
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox6_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
